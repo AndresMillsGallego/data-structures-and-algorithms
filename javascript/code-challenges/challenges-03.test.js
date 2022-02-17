8d5150eb9f2675d5aa0190f426b924dad340074e
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-16 17:24:54 -0800

    fixed whitespace issue

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -109,7 +109,6 @@ const snorlaxData = {
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
   // Solution code here...
   return arr.filter(stat => stat.baseStat > minBaseStat);
-  
 };
 
 /* ------------------------------------------------------------------------------------------------
