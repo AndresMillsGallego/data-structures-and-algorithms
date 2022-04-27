dc3280d7f2a8a0f1e234f110c5264650ebcfd93a
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-04-27 13:47:35 -0700

    2nd merge sort attempt

--- merge-sort/merge-sort.js
+++ merge-sort/merge-sort.js
@@ -1,48 +1,44 @@
 'use strict';
 
+
+function merge(left, right) {
+  let mergedArr = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      mergedArr.push(left.shift());
+    } else {
+      mergedArr.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    mergedArr.push(left.shift());
+  }
+  while (right.length) {
+    mergedArr.push(right.shift());
+  }
+  return mergedArr;
+}
+
 function mergeSort(arr) {
-  const n = arr.length;
+  let n = arr.length;
 
   if (n > 1) {
     const mid = Math.floor(n / 2);
     const left = arr.slice(0, mid);
     const right = arr.slice(mid);
-    mergeSort(left);
-    mergeSort(right);
 
-    return merge(left, right, arr);
+    const mergedLeft = mergeSort(left);
+    const mergedRight = mergeSort(right);
+
+    return merge(mergedLeft, mergedRight);
   }
 }
 
-function merge(left, right, arr) {
-  let i = 0;
-  let j = 0;
-  let k = 0;
-  while (i < left.length && j < right.length) {
-    if (left[i] <= right[j]) {
-      arr[k] = left[i];
-
-      i++;
-    } else {
-      arr[k] = right[j];
-      j++;
-    }
-    console.log(arr);
-    k++;
-  } return arr;
-  // if (i === left.length) {
-  //   return arr;
-  // } else {
-  //   return arr;
-  // }
-}
-
 let testArray = [8, 4, 23, 42, 16, 15];
-
 
 let results = mergeSort(testArray);
 console.log('results',results);
 
 module.exports = mergeSort;
-
-
