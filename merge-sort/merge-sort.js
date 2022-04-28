b0c0a13301e9828c71df01ae195735b48d173a59
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-04-27 17:16:31 -0700

    still trying this merge sort

--- merge-sort/merge-sort.js
+++ merge-sort/merge-sort.js
@@ -1,42 +1,45 @@
 'use strict';
 
+function mergeSort(arr) {
+
+  if (arr.length <= 1){
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  console.log(left, right)
+  return merge(left, right);
+
+}
+
 
 function merge(left, right) {
   let mergedArr = [];
 
   while (left.length && right.length) {
-    if (left[0] <= right[0]) {
+    if (left[0] < right[0]) {
       mergedArr.push(left.shift());
     } else {
       mergedArr.push(right.shift());
     }
+    return [...mergedArr, ...left, ...right];
   }
 
-  while (left.length) {
-    mergedArr.push(left.shift());
-  }
-  while (right.length) {
-    mergedArr.push(right.shift());
-  }
-  return mergedArr;
-}
-
-function mergeSort(arr) {
-  let n = arr.length;
-
-  if (n > 1) {
-    const mid = Math.floor(n / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
-
-    const mergedLeft = mergeSort(left);
-    const mergedRight = mergeSort(right);
-
-    return merge(mergedLeft, mergedRight);
-  }
+  // while (left.length) {
+  //   mergedArr.push(left.shift());
+  // }
+  // while (right.length) {
+  //   mergedArr.push(right.shift());
+  // }
+  // return mergedArr;
 }
 
 let testArray = [8, 4, 23, 42, 16, 15];
+
+let arr1 = [1,2,3];
+let arr2 = [4,5,6];
+console.log(merge(arr1, arr2));
 
 let results = mergeSort(testArray);
 console.log('results',results);
